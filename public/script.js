0ad89039be6e4159357556413c09532402cc8b9d
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-06 13:11:27 +0300

    append li and i element & click event

--- public/script.js
+++ public/script.js
@@ -1,92 +1,33 @@
 var sentence = prompt("Enter the sentence you want to read along");
 var words = sentence.split(' ');
 
-var word1 = document.querySelectorAll('.dot1');
-var word2 = document.querySelectorAll('.dot2');
-var word3 = document.querySelectorAll('.dot3');
-var word4 = document.querySelectorAll('.dot4');
-var word5 = document.querySelectorAll('.dot5');
-
-
-word1.forEach (function (element) {
-  element.addEventListener('click', () => {
-    let active = document.querySelector('.dot1');
-    active.classList.toggle('red');
-  });
-});
-
-word2.forEach (function (element) {
-  element.addEventListener('click', () => {
-    let active = document.querySelector('.dot2');
-    active.classList.toggle('red');
-  });
-});
-
-word3.forEach (function (element) {
-  element.addEventListener('click', () => {
-    let active = document.querySelector('.dot3');
-    active.classList.toggle('red');
-  });
-});
-
-word4.forEach (function (element) {
-  element.addEventListener('click', () => {
-    let active = document.querySelector('.dot4');
-    active.classList.toggle('red');
-  });
-});
-
-word5.forEach (function (element) {
-  element.addEventListener('click', () => {
-    let active = document.querySelector('.dot5');
-    active.classList.toggle('red');
-  });
-});
-
-
+/////////////////////
 //jQuery
 
 $(document).ready(() => {
 
 
-    //select the input
-    $( ".word1" )
-    .keyup(function() {
-      var value = $( this ).val();
-      $( ".word1" ).text(words[0]);
-    })
-    .keyup();
+    for(var i = 0; i < words.length; i++) {
 
-    $( ".word2" )
-    .keyup(function() {
-      var value = $( this ).val();
-      $( ".word2" ).text(words[1]);
-    })
-    .keyup();
+      //append li elements
+      $('.sentence').append(`<li class="${words[i]}">${words[i]}</li>`);
 
-    $( ".word3" )
-    .keyup(function() {
-      var value = $( this ).val();
-      $( ".word3" ).text(words[2]);
-    })
-    .keyup();
+      $( `${words[i]}` )
+      .keyup(function() {
+        var value = $( this ).val();
+        $( `.${words[i]}` ).text(words[i]);
+      })
+      .keyup();
 
-    $( ".word4" )
-    .keyup(function() {
-      var value = $( this ).val();
-      $( ".word4" ).text(words[3]);
-    })
-    .keyup();
+      //append i elements
+      $('.dots').append(`<li><i class="dot dot1${words[i]} far fa-hand-pointer"></i></li>`);
 
+      //add on Click event 
+      $(`.dot1${words[i]}`).on('click', function() {
+        $(this).toggleClass('red');
+      });
 
-
-
-
-  //select all span elements
-  var spanStyles = {
-    textDecoration: 'underline',
-    background: 'gray'
-  };
+    }
 
 
 });
